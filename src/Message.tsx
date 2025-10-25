37c21e4664d75e2846d16eb3996e4c84ea5ed8de
yassershahofficial <geniususer4u@gmail.com>
2025-10-25 22:24:01 +0800

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing
+function Message(){
+    //JSX : Javascript XML
+    const name = 'Yasser'
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message;
